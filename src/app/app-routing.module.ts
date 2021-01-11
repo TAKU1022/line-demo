feff648d72b2fc24d18695a0885374d5c1c7f3b3
Takumi Uchida <bmpni23786@yahoo.co.jp>
2021-01-11 15:23:00 +0900

    feat(app-routing): ルーティング追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadChildren: () => import('./top/top.module').then((m) => m.TopModule),
+  },
+  {
+    path: 'created-room-list',
+    loadChildren: () =>
+      import('./created-room-list/created-room-list.module').then(
+        (m) => m.CreatedRoomListModule
+      ),
+  },
+  {
+    path: 'joined-room-list',
+    loadChildren: () =>
+      import('./joined-room-list/joined-room-list.module').then(
+        (m) => m.JoinedRoomListModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
