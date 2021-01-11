7317589d082d1fd03f3a503f5f5ad8c66d48ffb0
Takumi Uchida <bmpni23786@yahoo.co.jp>
2021-01-11 20:59:01 +0900

    feat(app-routing): ガード追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
+import { GuestGuard } from './guards/guest.guard';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     loadChildren: () => import('./top/top.module').then((m) => m.TopModule),
+    canLoad: [AuthGuard],
+    canActivate: [AuthGuard],
   },
   {
     path: 'welcome',
     loadChildren: () =>
       import('./welcome/welcome.module').then((m) => m.WelcomeModule),
+    canLoad: [GuestGuard],
+    canActivate: [GuestGuard],
   },
   {
     path: 'created-room-list',
@@ -18,6 +24,8 @@ const routes: Routes = [
       import('./created-room-list/created-room-list.module').then(
         (m) => m.CreatedRoomListModule
       ),
+    canLoad: [AuthGuard],
+    canActivate: [AuthGuard],
   },
   {
     path: 'joined-room-list',
@@ -25,6 +33,8 @@ const routes: Routes = [
       import('./joined-room-list/joined-room-list.module').then(
         (m) => m.JoinedRoomListModule
       ),
+    canLoad: [AuthGuard],
+    canActivate: [AuthGuard],
   },
 ];
 
