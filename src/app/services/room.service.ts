0e2574880d727a3178362050a074a3d7030ab229
Takumi Uchida <bmpni23786@yahoo.co.jp>
2021-01-12 18:27:35 +0900

    feat(room.service): ルーム作成・参加追加

--- src/app/services/room.service.ts
+++ src/app/services/room.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Room } from '@interfaces/room';
+import firebase from 'firebase/app';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RoomService {
+  constructor(private db: AngularFirestore) {}
 
-  constructor() { }
+  createRoom(
+    room: Omit<Room, 'roomId' | 'createdAt' | 'updateAt'>
+  ): Promise<void> {
+    const roomId: string = this.db.createId();
+    return this.db.doc<Room>(`rooms/${roomId}`).set({
+      roomId,
+      createdAt: firebase.firestore.Timestamp.now(),
+      updateAt: firebase.firestore.Timestamp.now(),
+      ...room,
+    });
+  }
+
+  getAllRooms(): Observable<Room[]> {
+    return this.db.collection<Room>(`rooms`).valueChanges();
+  }
+
+  addRoomJoinedUserId(roomId: string, userId: string): Promise<void> {
+    return this.db
+      .doc(`rooms/${roomId}/joinedUserIds/${userId}`)
+      .set({ joinedUserId: userId });
+  }
 }
