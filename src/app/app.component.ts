c90a05fbfac4017eab03da47480ad56603f0987a
Takumi Uchida <bmpni23786@yahoo.co.jp>
2021-01-12 18:30:07 +0900

    feat(app): ユーザー情報表示

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { User } from '@interfaces/user';
+import { Observable } from 'rxjs';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'line-demo';
+  user$: Observable<User> = this.authService.user$;
+
+  constructor(private authService: AuthService) {}
 }
