8ef6d7542a8ac1dfd0d86d6c8f13f7143a891655
Takumi Uchida <bmpni23786@yahoo.co.jp>
2021-01-14 17:54:52 +0900

    feat(room-detail): ログ表示

--- src/app/room-detail/room-detail.module.ts
+++ src/app/room-detail/room-detail.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from '@angular/common';
 import { RoomDetailRoutingModule } from './room-detail-routing.module';
 import { RoomDetailComponent } from './room-detail/room-detail.component';
 import { SharedModule } from '../shared/shared.module';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [RoomDetailComponent],
-  imports: [CommonModule, RoomDetailRoutingModule, SharedModule],
+  imports: [
+    CommonModule,
+    RoomDetailRoutingModule,
+    SharedModule,
+    MatTableModule,
+  ],
 })
 export class RoomDetailModule {}
